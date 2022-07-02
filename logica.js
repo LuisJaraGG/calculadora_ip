d3866f52e85764d18e803d43fead8211c2071c77
unknown <luisalbarran21rl04091216@gmail.com>
2022-07-02 01:16:21 -0500

    terminado

--- logica.js
+++ logica.js
@@ -13,7 +13,7 @@ let broadcast = document.getElementById("broadcast");
 let primerip = document.getElementById("primeraip");
 let ultimoip = document.getElementById("ultimaip");
 let direciiones = document.getElementById("ipdisponibles");
-//adicionales hahahah knsjd
+//adicionales 
 
 let bitsclase = 0
 let clase ="";
